4207841bef3fba4eb591b955f3bf002266806c45
Sam Iavri <sammi80134@gmail.com>
2022-02-15 13:08:56 -0700

    Student model test for professor count

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,16 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-
   describe 'validations' do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :age}
-    it {should validate_presence_of :house}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :age }
+    it { should validate_presence_of :house }
   end
 
   describe 'relationships' do
-    it {should have_many :professor_students}
-    it {should have_many(:professors).through(:professor_students)}
+    it { should have_many :professor_students }
+    it { should have_many(:professors).through(:professor_students) }
   end
 
+  it 'counts students professors' do
+    snape = Professor.create(name: 'Severus Snape', age: 45, specialty: 'Potions')
+    hagarid = Professor.create(name: 'Rubeus Hagrid', age: 38, specialty: 'Care of Magical Creatures')
+    lupin = Professor.create(name: 'Remus Lupin', age: 49, specialty: 'Defense Against The Dark Arts')
+
+    harry = Student.create(name: 'Harry Potter', age: 11, house: 'Gryffindor')
+
+    ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+
+    expect(harry.professor_count).to eq(3)
+  end
 end
